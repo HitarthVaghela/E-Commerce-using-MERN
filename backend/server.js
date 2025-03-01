57cf100508211ae088c40285b37b874505787a03
HitarthVaghela <hdvaghela27@gmail.com>
2025-03-01 11:55:05 +0530

    Removed package-lock.json from node_modules tracking

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res) => {
+app.post("/products", (req,res) => {
     // res.send("Server is ready");
+    
 })
 
 // console.log(process.env.MONGO_URI);
