fd316111edcc27204548bc354c83e72107b809e1
HitarthVaghela <hdvaghela27@gmail.com>
2025-03-05 19:00:15 +0530

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ChakraProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
